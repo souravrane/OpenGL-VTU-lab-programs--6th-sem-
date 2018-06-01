460a502bea9b30e861ce02fc3cb2a4396635cc87
Sourav Rane <sourav1121997@gmail.com>
2018-06-01 10:42:28 +0530

    reduced code
    reduced code

--- 8. Teapot/teapot.c
+++ 8. Teapot/teapot.c
@@ -1,8 +1,7 @@
-
 #include <stdio.h>
 #include <GL/glut.h>
 
-GLfloat x = 2.3, y = 1.3;
+GLfloat x = 2.3, y = 1.0;
 
 void wall(double thickness)
 {
@@ -47,39 +46,25 @@ void table(double topWid, double topThick, double legThick, double legLen)
 
 void displaySolid()
 {
-	//surface material
-	GLfloat mat_ambient[] = { 0.7f,0.7f,0.7f,1.0f };
-	GLfloat mat_diffuse[] = { 0.5f,0.5f,0.5f,1.0f };
-	GLfloat mat_specular[] = { 1.0f,1.0f,1.0f,1.0f };
-	GLfloat mat_shininess[] = { 50.0f };
-	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-
-
-
 	//light source
-	GLfloat lightIntensity[] = { 0.7f,0.7f,0.7f,1.0f };
+	GLfloat lightIntensity[] = { 1.0f,1.0f,1.0f,1.0f };
 	GLfloat lightPosition[] = { 2.0f,6.0f,3.0f,0.0f };
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
 
 
-
 	//camera positioning
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	double winHt = 1.0;        //half-height of window
-	glOrtho(-winHt * 64 / 48.0, winHt * 64 / 48.0, -winHt, winHt, 0.1, 100.0);
+	glOrtho(-1, 1, -1, 1, 1, 100.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(x, y, 2.0, 0.0, 0.25, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(x, y, 2.0, 0.0, 0.2, 0.0, 0.0, 1.0, 0.0);
 
 	//start drawing
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
-	glTranslated(0.6, 0.38, 0.5);
+	glTranslated(0.5, 0.4, 0.5);
 	glRotated(30, 0, 1, 0);
 	glutSolidTeapot(0.08);
 	glPopMatrix();
@@ -106,29 +91,22 @@ void displaySolid()
 	glFlush();
 }
 
-void motion(int x1, int y1)
-{
-	x = x1 / 100;
-	y = y1 / 100;
-}
 
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(640, 480);
+	glutInitWindowSize(640, 640);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Teapot -- Arpith");
+	glutCreateWindow("Teapot");
 	glutDisplayFunc(displaySolid);
-	glutMotionFunc(motion);
+	
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
-	glShadeModel(GL_SMOOTH);
+	
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_NORMALIZE);
 	glClearColor(0.1, 0.1, 0.1, 0.0);
-	glViewport(0, 0, 640, 480);
 	glutMainLoop();
 	return 0;
 }
-
